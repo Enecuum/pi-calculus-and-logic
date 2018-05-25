063edb178382f1511af06236472bd523c0ca11e7
OAZZZIZ <o@zzz.iz>
2018-05-25 23:55:38 +0300

    changes

--- Data/PiCalculusAndLogic/Parser.hs
+++ Data/PiCalculusAndLogic/Parser.hs
@@ -13,16 +13,32 @@ channel = do
   b <- many alphaNum
   return $ fromString (a:b)
 
-term = fail <|> empt <|> block
+term = comm <|> ordr <|> serv <|> optf <|> fail <|> empt <|> log2 <|> block
 
 block :: Parser Term
 block = undefined
 
+pattern :: Parser (Either String Chan)
+pattern = (do a <- oneOf "*?"; return $ Left a) <|> (do a <- channel; return $ Right a)
+
 send :: Parser Term
-send = undefined
+send = do
+  ma <- optionMaybe channel
+  char '<'
+  mb <- optionMaybe pattern
+  char '>'
+  case (ma,mb) of
+    (Nothing,Nothing)         -> parserZero
+    (Just  a,Just (Left "?")) -> parserZero
+    (Just  a,Just (Left "*")) -> parserZero
+    (Nothing,Just (Left "?")) -> parserZero
+    (Nothing,Just (Left "*")) -> parserZero
+    (Nothing,Just (Right  b)) -> parserZero
+    (Just  a,Just (Right  b)) -> Send a b
 
 wait :: Parser Term
-wait = undefined
+wait = do
+  ma <- optionMaybe channel
 
 comm :: Parser Term
 comm = undefined
