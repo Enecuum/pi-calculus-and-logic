4d130466ed7e3269234195d2e0f706d3a7ef5616
OAZZZIZ <o@zzz.iz>
2018-05-29 23:47:26 +0300

    changes

--- Data/PiCalculusAndLogic/Term.hs
+++ Data/PiCalculusAndLogic/Term.hs
@@ -6,10 +6,11 @@ import Data.SymbolForChan
 
 data Term = Empt | Fail
 
-
+          -- Operators for classic pi calculus
           | FPUB { questToCommPipe    :: Chan, useBindFromPast     :: Chan }    -- use bind from past     quest < bfp >
           | BTRS { listenFromCommPipe :: Chan, newBindToRightSide  :: Chan }    -- bind to right side     listen ( newBindTRS )
 
+          -- Operators for offer choosing
           | UBFF { useBindFromFuture  :: Chan, chooseAlterFromPipe :: Chan }    -- use bind from future   ( bff ) choose
           | LSBT { newBindToLeftSide  :: Chan, offerAlterToPipe    :: Chan }    -- bind to left side      < newBindTLS > offer
 
@@ -26,12 +27,27 @@ data Term = Empt | Fail
           -- With channel communication
           | OPTF {                                 termToRemoveOrKeep :: Term } -- optional term for remove on fail
           | OPTS { termForSuccessOrRemove :: Term, termToRemoveOrKeep :: Term } -- optional term for remove after success
+
+          | PATT [Term] -- if less term in order is success after fail of previous terms all terms is removed except this one
+          | POOL [Term] -- greather terms in order must use duplication less than first in order
           
           -- Without channel communication between terms inside this operators
-          | PLUS [Term] -- success one or more to success
-          | CONJ [Term] -- fail one or more to fail
+          | PLUS [Term] -- success one or more to success, other term is not forces to remove for this time
+          | CONJ [Term] -- quick fail one or more to fail, success is not forced to check for this time
           | PROD [Term] -- success only if every term is succesefull
           | DISJ [Term] -- success only if every term is fail but only one is success
+
+          -- Security operators
+
+          | ISOL  Term  -- isolate all channel communications between terms inside PATT POOL ORDR in first layer
+                        --                                               or inside OPTF OPTS(tfsor) second layer
+
+          | MOBI  Term  -- all channel communication must be mobile, input  channel must get output channel only
+                        --                                       and output channel must put input  channel only
+
+          -- Foreign terms and tools
+          | PROX       Dynamic -- proxy for dynamic from haskell
+          | DBUG  Term Dynamic -- use dynamics for term debugging
 
 
  deriving (Eq,Ord)
