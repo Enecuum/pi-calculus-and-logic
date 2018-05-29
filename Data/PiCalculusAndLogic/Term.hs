cfae12eee3789bc85536c458e6b96279ffc773cd
OAZZZIZ <o@zzz.iz>
2018-05-29 18:27:57 +0300

    changes

--- Data/PiCalculusAndLogic/Term.hs
+++ Data/PiCalculusAndLogic/Term.hs
@@ -5,8 +5,21 @@ module Data.PiCalculusAndLogic.Term where
 import Data.SymbolForChan
 
 data Term = Empt | Fail
-          | Send { chanToPipe :: Chan, nameToSend :: Chan }
-          | Wait { chanToWait :: Chan, nameToBind :: Chan }
+
+
+          | Ques { quesToPipe :: Chan, bindToSend :: Chan }  -- a < b >
+          | Wait { chanToWait :: Chan, nameToBind :: Chan }  -- a ( b )   bindToRightSide
+
+
+          | Send { nextToBind :: Chan, alterToSend :: Chan } -- ( a ) [ b ]
+          | Recv { bindToWait :: Chan, nameToAlter :: Chan } -- < a > [ b ]  bindToLeftSide
+
+          | UseBindFromPast   { questToCommPipe   :: Chan, bindToUseAndSendIt :: Chan }
+          | UseBindFromFuture { useBindFromFuture :: Chan, alterToSend        :: Chan }
+
+          | BindToRightSide { chanToWait :: Chan, nameToBind :: Chan }
+          | BindToLeftSide  { bindToWait :: Chan, nameToChan :: Chan }
+
           | OptW { chanToWait :: Chan }
           | OptF  Term
           | Comm [Term]
