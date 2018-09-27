4378ed1bfb5cdb2ebe15fdb07eedcce08b699abe
OAZZZIZ <o@zzz.iz>
2018-09-28 00:03:54 +0300

    changes

--- test01.hs
+++ test01.hs
@@ -3,22 +3,35 @@ import Control.Concurrent.STM.TVar
 import System.IO.Unsafe
 
 data Term
- = MROOT
- | AUNIT
- | MUNIT
- | Term `CMCNJ` Term
- | Term `CMDSJ` Term
- | Term `NMCNJ` Term
- | Term `CADSJ` Term
- | Term `CACNJ` Term
- | INVRT Term
- | NEGAT Term
- | OFCRS Term
- | WHYNT Term
+ = MROOT -- Multiplicative root of top formula, expresion must be equal to multiplicative unit for success
+ | AUNIT -- Additive unit, a+0 == a
+ | MUNIT -- Multiplicative unit, a*1 == a, or for example matrix * identitiy matrix for non commutative case
+ | Term `CMCNJ` Term -- Commutative Multiplicative Conjunction
+ | Term `CMDSJ` Term -- Commutative Multiplicative Disjunction
+ | Term `NMCNJ` Term -- Non-Commutative Associative Multiplicative Conjunction (like matrix multiplication for example)
+ | Term `CADSJ` Term -- Commutative Additive Disjunction
+ | Term `CACNJ` Term -- Commutative Additive Conjunction
+ | INVRT Term -- Multiplicative inverse, a^-1 or like matrix inverse for non commutative
+ | NEGAT Term -- Additive negatation, negative numbers for example
+ | OFCRS Term -- Of-course exponential operator
+ | WHYNT Term -- Why-not exponential operator
 
  | Atom Descr
 
  deriving (Show,Eq)
+
+-- predicate -> termFunctorM -> originalTerm -> m transformatedTerm
+termMapM :: Monad m => (Term -> Bool) -> (Term -> m Term) -> Term -> m Term
+termMapM p f o@(a `CMCNJ` b) | p o = do c <- f a; d <- f b; return (c `CMCNJ` d)
+termMapM p f o@(a `CMDSJ` b) | p o = do c <- f a; d <- f b; return (c `CMDSJ` d)
+termMapM p f o@(a `NMCNJ` b) | p o = do c <- f a; d <- f b; return (c `NMCNJ` d)
+termMapM p f o@(a `CADSJ` b) | p o = do c <- f a; d <- f b; return (c `CADSJ` d)
+termMapM p f o@(a `CACNJ` b) | p o = do c <- f a; d <- f b; return (c `CACNJ` d)
+termMapM p f o@(INVRT a) | p o = do b <- f a; return (INVRT b)
+termMapM p f o@(NEGAT a) | p o = do b <- f a; return (NEGAT b)
+termMapM p f o@(OFCRS a) | p o = do b <- f a; return (OFCRS b)
+termMapM p f o@(WHYNT a) | p o = do b <- f a; return (WHYNT b)
+termMapM _ _ o = return o
 
 data Pattern = ConstName String | ConstId Integer | UniqueId Integer | UniqueWildcard Integer
  deriving (Eq)
