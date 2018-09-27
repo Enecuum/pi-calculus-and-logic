a5f4a8ead1096ca8ea103e2638e8c6339d18d895
OAZZZIZ <o@zzz.iz>
2018-09-27 23:01:54 +0300

    changes

--- test01.hs
+++ test01.hs
@@ -1,5 +1,6 @@
 
 import Control.Concurrent.STM.TVar
+import System.IO.Unsafe
 
 data Term
  = MROOT
@@ -12,11 +13,26 @@ data Term
  | Term `CACNJ` Term
  | INVRT Term
  | NEGAT Term
+ | OFCRS Term
+ | WHYNT Term
 
 
  | Atom Descr
+ deriving (Show,Eq)
 
 data Descr = Descr { path :: [Maybe (Either String Integer)], isReduced :: TVar (Maybe Integer) }
+ deriving (Eq)
+
+instance Show Descr where
+  show (Descr a b) = concat $ map (':':) $ map f a
+   where
+    f Nothing = "*"
+    f (Just (Left a)) = a
+    f (Just (Right a)) = show a
+
+at x = unsafePerformIO $ do
+  tv <- newTVarIO Nothing
+  return $ Atom $ Descr [Just $ Left x] tv
 
 
 
