e2595c36a44a1bd1df5addb76aab8af6189427b3
OAZZZIZ <o@zzz.iz>
2018-09-28 23:23:33 +0300

    changes

--- test01.hs
+++ test01.hs
@@ -27,6 +27,8 @@ data Term
 
  deriving (Show,Eq)
 
+tupleMap f (a,b) = (f a, f b)
+
 -- predicate -> termFunctorM -> originalTerm -> m transformatedTerm
 termMapM :: Monad m => (Term -> Bool) -> (Term -> m Term) -> Term -> m Term
 termMapM p f o@(a `CMCNJ` b) | p o = do c <- f a; d <- f b; return (c `CMCNJ` d)
@@ -95,7 +97,10 @@ instance FocusUp Term where
     f a | isFocusExistInTermOp a = Focus a
     f a = a
 
+focup01 a = map (tupleMap focusUp) a
+
 test01 = [(at "q" `CMCNJ` (Focus (at "w") `CMCNJ` at "e"), at "r" `CMCNJ` at "t")]
+test02 = [(at "q" `CMCNJ` (Focus (at "w") `CMCNJ` Focus (at "e")), at "r" `CMCNJ` at "t")]
 
 data UniqueShare = USH Integer
 
@@ -130,6 +135,8 @@ at x = unsafePerformIO $ do
 
 rotateFocus [] = []
 
+rotateFocus [(Focus a,b)] = rotateFocus [(a,b)]
+
 rotateFocus o@[(a,b)] | isJust c = case d of
   (Focus g, Focus h) -> unsafePerformIO $ do
     shH <- newUniqueShare
@@ -137,13 +144,15 @@ rotateFocus o@[(a,b)] | isJust c = case d of
     let rot02 = rotateFocus [(h, shH)] -- term h equal to shH in sense of connected shares
     return $ rot01 ++ rot02
   (Focus g, h) -> rotateFocus [(g, e b $ f h)]
-  (g, Focus h) -> rotateFocus [(h, e (f g) h)]
+  (g, Focus h) -> rotateFocus [(h, e (f g) b)]
   _ -> o
  where
   c = toBinop a
   Just (d,(e,f)) = c
 
-rotateFocus (a:b) = rotateFocus [a] ++ rotateFocus b
+rotateFocus (a:b:c) = rotateFocus [a] ++ rotateFocus (b:c)
+
+rotateFocus a = a
 
 {-
 
