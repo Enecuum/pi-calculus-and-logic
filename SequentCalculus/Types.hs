eb898e25671e9eafc1f55f927c7675087154fd6c
OAZZZIZ <o@zzz.iz>
2018-10-05 16:35:25 +0300

    changes

--- SequentCalculus/Types.hs
+++ SequentCalculus/Types.hs
@@ -12,12 +12,14 @@ module SequentCalculus.Types where
 data Side
 data Sequent
 data NestedSequent
+data HyperSequent
 data Rule
 
 class RuleSide a where
 
 instance RuleSide Sequent
 instance RuleSide NestedSequent
+instance RuleSide HyperSequent
 
 data Sqcalc t a where
   Lift  :: t (Sqcalc t Side)                                    -> Sqcalc t Side
@@ -26,6 +28,7 @@ data Sqcalc t a where
   Turns ::    Sqcalc t Side    -> Sqcalc t Side                 -> Sqcalc t Sequent
   Sqset ::   [Sqcalc t Sequent]                                 -> Sqcalc t Sequent
   NsBox ::  RuleSide a => Sqcalc t a -> Int                     -> Sqcalc t NestedSequent
+  Hyper ::   [Sqcalc t Sequent]                                 -> Sqcalc t HyperSequent
   Infer :: (RuleSide a, RuleSide b) => Sqcalc t a -> Sqcalc t b -> Sqcalc t Rule
 
 infixr 8 `Turns`
