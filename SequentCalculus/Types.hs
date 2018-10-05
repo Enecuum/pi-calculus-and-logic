b9e21c2f23ea8641cbd9c2fe1d45a807727c8ad9
OAZZZIZ <o@zzz.iz>
2018-10-05 15:23:06 +0300

    changes

--- SequentCalculus/Types.hs
+++ SequentCalculus/Types.hs
@@ -1,13 +1,34 @@
+{-# LANGUAGE GADTs #-}
+
 module SequentCalculus.Types where
 
-data Sqcalc a where
+data Side
+data Sequent
+data Rule
 
+data Sqcalc t a where
+  Lift  :: t (Sqcalc t Side)                       -> Sqcalc t Side
+  Tack  ::    Sqcalc t Side    -> Sqcalc t Side    -> Sqcalc t Side
+  Sdset ::   [Sqcalc t Side]                       -> Sqcalc t Side
+  Turns ::    Sqcalc t Side    -> Sqcalc t Side    -> Sqcalc t Sequent
+  Sqset ::   [Sqcalc t Sequent]                    -> Sqcalc t Sequent
+  Infer ::    Sqcalc t Sequent -> Sqcalc t Sequent -> Sqcalc t Rule
 
-  Tack  ::  Sqcalc Side    -> Sqcalc Side    -> Sqcalc Side
-  Sdset :: [Sqcalc Side]                     -> Sqcalc Side
-  Turns ::  Sqcalc Side    -> Sqcalc Side    -> Sqcalc Sequent
-  Sqset :: [Sqcalc Sequent]                  -> Sqcalc Sequent
-  Infer ::  Sqcalc Sequent -> Sqcalc Sequent -> Sqcalc Rule
+data Classic a where
+  Elem  :: a                      -> Classic a
+  Indet :: String                 -> Classic a
+  Conj  :: Classic a -> Classic a -> Classic a
+  Disj  :: Classic a -> Classic a -> Classic a
+  Not   :: Classic a              -> Classic a
+
+infixr 8 `Turns`
+infixr 7 `Infer`
+
+classicRules =
+  [         Sdset [Lift (Indet "Γ"), Lift (Indet "A" `Conj` Indet "B")] `Turns` Lift (Indet "Δ")
+    `Infer` -------------------------------------------------------------------------------------
+            Sdset [Lift (Indet "Γ"), Lift (Indet "A"), Lift (Indet "B")] `Turns` Lift (Indet "Δ")
+  ]
 
 
 
