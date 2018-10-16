90beb966b55eadf647de3c86d7c75153010e9356
OAZZZIZ <o@zzz.iz>
2018-10-17 00:38:59 +0300

    changes

--- MorphismCalculus/Term.hs
+++ MorphismCalculus/Term.hs
@@ -2,7 +2,20 @@
 
 module MorphismCalculus where
 
+data Composition a b
+data Ident
+data Zero
+data Empty
+
 class Morphism a where
+
+class Set a where
+
+instance Set (Category a) where
+
+instance (Morphism a, Morphism b) => Morphism (Composition a b) where
+instance Morphism Ident where
+instance Morphism Zero where
 
 data Category a where
  Conjunction :: Category a -> Category a -> Category a
@@ -10,3 +23,8 @@ data Category a where
  Product     :: Category a -> Category a -> Category a
  CoProduct   :: Category a -> Category a -> Category a
  Category    :: Morphism m => { morphism :: Category m, domain :: Category a, codomain :: Category a } -> Category b
+ Composition :: (Morphism a, Morphism b) => Category a -> Category b -> Category (Composition a b)
+ Ident       :: Category Ident
+ Zero        :: Category Zero
+ Empty       :: Category Empty
+ Union       :: Set a => Category a -> Category a -> Category a
