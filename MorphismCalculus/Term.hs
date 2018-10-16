521d505301a8a4e32cc8f40b01c3177767543a52
OAZZZIZ <o@zzz.iz>
2018-10-17 00:05:03 +0300

    changes

---
+++ MorphismCalculus/Term.hs
@@ -0,0 +1,12 @@
+{-# LANGUAGE GADTs #-}
+
+module MorphismCalculus where
+
+class Morphism a where
+
+data Category a where
+ Conjunction :: Category a -> Category a -> Category a
+ Disjunction :: Category a -> Category a -> Category a
+ Product     :: Category a -> Category a -> Category a
+ CoProduct   :: Category a -> Category a -> Category a
+ Category    :: Morphism m => Category a -> Category m -> Category a -> Category b
