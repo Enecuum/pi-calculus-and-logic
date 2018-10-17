2eeaed1ae8dae63daa6659263dbd6c4c89aaffb3
OAZZZIZ <o@zzz.iz>
2018-10-17 15:45:45 +0300

    changes

---
+++ MorphismCalculus/Term02.hs
@@ -0,0 +1,16 @@
+module MorphismCalculus.Term2 where
+
+data Term a
+   = Idnt
+   | Empt
+   | Conj (Term a) (Term a)
+   | Disj (Term a) (Term a)
+   | Prod (Term a) (Term a)
+   | Mdsj (Term a) (Term a)
+   | Comp (Term a) (Term a)
+   | Unio (Term a) (Term a)
+   | Name [String]
+   | Morp { morphism :: Term a, domain :: Term a, codomain :: Term a }
+
+
+
