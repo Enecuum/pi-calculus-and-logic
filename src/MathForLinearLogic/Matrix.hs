1dadc9c0646ffdb8459e346cad53df3bd013e5b9
OAZZZIZ <o@zzz.iz>
2018-11-08 09:11:52 +0300

    changes

--- src/MathForLinearLogic/Matrix.hs
+++ src/MathForLinearLogic/Matrix.hs
@@ -1,12 +1,83 @@
+{-# LANGUAGE DataKinds, TypeOperators, KindSignatures, TypeFamilies, UndecidableInstances, ConstrainedClassMethods, AllowAmbiguousTypes, FlexibleInstances, MultiParamTypeClasses, FunctionalDependencies, FlexibleContexts #-}
+
 module MathForLinearLogic.Matrix where
 
-import Data.Matrix
+import Data.Matrix hiding (Matrix)
+import qualified Data.Matrix as M
 import System.Random
 import Control.Monad
 import Data.List
 import Math.NumberTheory.Primes.Factorisation
 import System.Random.Shuffle
 import Math.NumberTheory.Primes.Testing
+import GHC.TypeLits
+import Data.Proxy
+
+type Matrix7det32type01 = Matrix 7 4194476831
+
+newtype Matrix (size :: Nat) (prime :: Nat) = Matrix (Vec size (Vec size Integer))
+
+data a :. b = a :. b
+
+infixr 8 :.
+
+type family Vec (a :: Nat) b where
+  Vec 0 a = ()
+  Vec 1 a = a :. ()
+  Vec n a = a :. Vec (n-1) a
+
+type family CellNum a where
+  CellNum (Matrix a b) = Div (b+1) a
+
+type family DiagCellNum a where
+  DiagCellNum a = CellNum a - 1
+
+test :: Vec 10 Integer
+test = undefined
+
+test01 :: Proxy (CellNum (Matrix7det32type01))
+test01 = undefined
+
+test02 :: Proxy (DiagCellNum (Matrix7det32type01))
+test02 = undefined
+
+test03 :: Vec 4 Double
+test03 = 1 :. 2 :. 3 :. 4 :. ()
+
+class VecToList a b where
+  vecToList :: a -> [b]
+
+instance VecToList () b where
+  vecToList () = []
+
+instance VecToList b a => VecToList (a :. b) a where
+  vecToList (a :. b) = a : vecToList b
+
+instance (VecToList b a, VecToList (a :. b) a, Show a) => Show (a :. b) where
+  show (a :. b) = show ( a : vecToList b )
+
+--matrixToLists :: (KnownNat a, KnownNat b, VecToList (Vec a (Vec a Integer)) (Vec a Integer), VecToList (Vec a Integer) Integer) => Matrix a b -> [[Integer]]
+matrixToTab :: (KnownNat a, KnownNat b, VecToList (Vec a (Vec a Integer)) (Vec a Integer)) => Matrix a b -> [Vec a Integer]
+matrixToTab (Matrix a) = vecToList a
+
+matrixToLists :: (KnownNat a, KnownNat b, VecToList (Vec a Integer) Integer, VecToList (Vec a (Vec a Integer)) (Vec a Integer)) => Matrix a b -> [[Integer]]
+matrixToLists a = map vecToList $ matrixToTab a
+
+toDataMatrix a = fromLists $ matrixToLists a
+
+testMatrix :: Matrix 2 127
+testMatrix = Matrix (  (1 :. 2 :. ())  :.  (3 :. 4 :. ()) :. () )
+
+instance (KnownNat a, KnownNat b, VecToList (Vec a Integer) Integer, VecToList (Vec a (Vec a Integer)) (Vec a Integer)) => Show (Matrix a b) where
+  show a = show $ toDataMatrix a
+
+{-
+
+*MathForLinearLogic.Matrix> randomCyclicMatrixPrime 32 7
+(31.96584373130438,(4194476831,599210975),4194476831)
+(31.96584373130438,(4194476831,599210975))
+
+-}
 
 
 rotateList :: [a] -> [a]
@@ -36,6 +107,7 @@ randomCyclicMatrixPrime scale size = do
     --(_,True,_,_) -> print (det2) >> return (log (realToFrac det) / log 2, (det, c))
     _ -> randomCyclicMatrixPrime scale size
 
+{-
 randomCyclicMatrix :: Double -> Int -> IO (Matrix Integer)
 randomCyclicMatrix scale size = do
   let a = 2**scale
@@ -56,4 +128,5 @@ randomPrimeMatrix scale size = do
     (True,True,_) -> return (log (realToFrac b) / log 2, b, head $ head $ toLists a)
     --(True,_,[(_,1)]) -> print b >> return a
     _ -> randomPrimeMatrix scale size
+-}
 
