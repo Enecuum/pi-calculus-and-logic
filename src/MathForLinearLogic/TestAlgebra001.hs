7dd373171e0a720e23f05ca9ece358d16c3162a7
OAZZZIZ <o@zzz.iz>
2018-12-01 12:54:20 +0300

    changes

--- src/MathForLinearLogic/TestAlgebra001.hs
+++ src/MathForLinearLogic/TestAlgebra001.hs
@@ -1,25 +1,53 @@
-{-# LANGUAGE GADTs, ExistentialQuantification, FlexibleInstances, DatatypeContexts, StandaloneDeriving, UndecidableInstances #-}
+{-# LANGUAGE GADTs, ExistentialQuantification, FlexibleInstances, DatatypeContexts, StandaloneDeriving, UndecidableInstances, TypeFamilies #-}
 
 module MathForLinearLogic.TestAlgebra001 where
 
-newtype FixF f = InF { outF :: f (FixF f) }
+newtype FixF f = InF ( f (FixF f) )
 
 instance Show (f (FixF f)) => Show (FixF f) where
   show (InF a) = show a
 
-type Term = TermF TermA
-type TermA = FixF (TermBF Integer)
+type Term = FixF (TermBF Integer)
 type TermF a = TermBF Integer a
 
-data (a ~ Integer, b ~ TermA) => TermBF a b
-  = U | W | N Integer
-  | Term `Add` Term
-  | Term `Sub` Term
-  | Term `Mul` Term
-  | Term `Div` Term
-  | Term `Mod` Integer
-  | Term `Pow` Integer
-deriving instance Show Term
+{-
+type family (OutF a) where
+  OutF (FixF f) = f (FixF f)
+-}
+
+class Fixable a where
+  type OutF a
+  inF :: OutF a -> a
+  outF :: a -> OutF a
+
+instance Fixable (FixF f) where
+  type OutF (FixF f) = f (FixF f)
+  inF = InF
+  outF (InF a) = a
+
+data TermBF a b where
+  U :: TermBF a b
+  W :: TermBF a b
+  N :: a -> TermBF a b
+  Add :: Fixable b => OutF b -> OutF b  -> TermBF a (FixF (TermBF a))
+  Sub :: Fixable b => OutF b -> OutF b  -> TermBF a b
+  Mul :: Fixable b => OutF b -> OutF b  -> TermBF a b
+  Div :: Fixable b => OutF b -> OutF b  -> TermBF a b
+  Mod :: Fixable b => OutF b -> a -> TermBF a b
+  Pow :: Fixable b => OutF b -> a -> TermBF a b
+
+{-
+data Fixable b => TermBF a b
+  = U | W | N a
+  | OutF b `Add` OutF b
+  | OutF b `Sub` OutF b
+  | OutF b `Mul` OutF b
+  | OutF b `Div` OutF b
+  | OutF b `Mod` a
+  | OutF b `Pow` a
+-}
+
+deriving instance Show (TermF Term)
 
 infixr 7 `Add`
 infixr 7 `Sub`
@@ -27,21 +55,70 @@ infixr 8 `Mul`
 infixr 8 `Div`
 
 n :: Integer -> Term
-n x = N x
+n x = InF $ N x
 
 instance Num Term where
-  a - b = reduce (a `Sub` b)
-  a + b = reduce (a `Add` b)
-  a * b = reduce (a `Mul` b)
-  fromInteger n = N n
+  a - b = reduce $ InF (outF a `Sub` outF b)
+  a + b = reduce $ InF (outF a `Add` outF b)
+  a * b = reduce $ InF (outF a `Mul` outF b)
+  fromInteger n = InF $ N n
 
 reduce :: Term -> Term
 reduce a = a
 
+termCondBimapM :: Monad m => (TermBF a b -> Bool) -> (a -> m a) -> (b -> m b) -> TermBF a b -> m (TermBF a b)
+termCondBimapM = undefined
+
 {-
 termCondBimapM :: Monad m => (Term -> Bool) -> (Integer -> m Integer) -> (Term -> m Term) -> Term -> m Term
-termCondBimapM p f j o@(a `Add` b) | p o = do c <- j a; d <- j a; return (c `Add` d)
-termCondBimapM p f j o@(N a) | p o = do b <- f a; return (N b)
+termCondBimapM p f j o@(InF (a `Add` b)) | p o = do InF c <- j (InF a); InF d <- j (InF b); return (InF (c `Add` d))
+termCondBimapM p f j o@(InF (a `Sub` b)) | p o = do InF c <- j (InF a); InF d <- j (InF b); return (InF (c `Sub` d))
+termCondBimapM p f j o@(InF (a `Mul` b)) | p o = do InF c <- j (InF a); InF d <- j (InF b); return (InF (c `Mul` d))
+termCondBimapM p f j o@(InF (a `Div` b)) | p o = do InF c <- j (InF a); InF d <- j (InF b); return (InF (c `Div` d))
+termCondBimapM p f j o@(InF (a `Mod` b)) | p o = do InF c <- j (InF a);     d <- f      b ; return (InF (c `Mod` d))
+termCondBimapM p f j o@(InF (a `Pow` b)) | p o = do InF c <- j (InF a);     d <- f      b ; return (InF (c `Pow` d))
+termCondBimapM p f j o@(InF (N a)) | p o = do b <- f a; return (InF (N b))
+-}
+
+class CondBifunctorM t where
+  -- type OutF (FixF (t a))
+  condBimapM :: Monad m => (t a b -> Bool) -> (a -> m c) -> (b -> m d) -> t a b -> m (t c d)
+
+--instance (OutF (FixF (TermBF a)) ~ TermBF a (FixF (TermBF a))) => CondBifunctorM TermBF where
+--instance (TermBF a b, OutF b ~ FixF (TermBF a)) => CondBifunctorM (TermBF :: * -> * -> *) where
+instance CondBifunctorM TermBF where
+  -- type OutF (FixF (TermBF a)) = TermBF a (FixF (TermBF a))
+  condBimapM p f j o@(a `Pow` b) | p o = do c <- j (inF a); d <- f b; return (outF c `Pow` d)
+  condBimapM p f j o@(N a) | p o = do b <- f a; return (N b)
+
+-- TermBF OutF b -> b
+
+
+-- cataM :: (CondBifunctorM t, Monad m) => (t a (FixF (t a)) -> Bool) -> (FixF (t a) -> m b) -> FixF (t a) -> m b
+-- cataM p f a = condBimapM p return ((condBimapM p return f . outF) >>= (return . InF)) (outF a) >>= f
+
+{-
+termAnaM :: Monad m => (Term -> Bool) -> (Term -> m Term) -> Term -> m Term
+termAnaM p f a = f a >>= termMapM p (termAnaM p f)
+
+termHyloM :: Monad m => (Term -> Bool) -> (Term -> m Term) -> (Term -> m Term) -> Term -> m Term
+termHyloM p f g a = g a >>= termMapM p (termHyloM p f g) >>= f
+
+termParaM :: Monad m => (Term -> Bool) -> ((Term, Term) -> m Term) -> Term -> m Term
+termParaM p f a = termMapM p (termParaM p f) a >>= curry f a
+
+termApoM :: Monad m => (Term -> Bool) -> (Term -> m (Either Term Term)) -> Term -> m Term
+termApoM p f a = do
+  b <- f a
+  case b of
+    Left  c -> return c
+    Right d -> termMapM p (termApoM p f) d
+
+termCata p f   a = runIdentity $ termCataM p (return . f) a
+termAna  p f   a = runIdentity $ termAnaM  p (return . f) a
+termHylo p f g a = runIdentity $ termHyloM p (return . f) (return . g) a
+termPara p f   a = runIdentity $ termParaM p (return . f) a
+termApo  p f   a = runIdentity $ termApoM  p (return . f) a
 -}
 
 
