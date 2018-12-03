d0573f5d82524ff77acf02e123db854cb9da6878
OAZZZIZ <o@zzz.iz>
2018-12-04 00:39:39 +0300

    changes

--- src/PiCalculusClassic/Expr.hs
+++ src/PiCalculusClassic/Expr.hs
@@ -22,18 +22,19 @@ data ExprBF a b
    | Scop { newChannelName :: (T "Name" a), process :: OutF b }
    | OutF b `Comm` OutF b
    | Serv (OutF b)
+   -- | Test (OutF (T "Test" a))
 
 deriving instance Show Expr
 deriving instance Show ExprA
 
 instance CondBifunctorM ExprBF where
   type FirstPrototype ExprBF = (R "Value" AnyType :@ R "Name" AnyType)
-  condBimapM p f j o@(Value a)    | p o = do b <- f (to @"Value" a); return $ Value (fr @"Value" b)
-  condBimapM p f j o@(Send a b c) | p o = do d <- f (to @"Name" a); e <- f (to @"Name" b); g <- j (inF c); return $ Send (fr @"Name" d) (fr @"Name" e) (outF g)
-  condBimapM p f j o@(Recv a b c) | p o = do d <- f (to @"Name" a); e <- f (to @"Name" b); g <- j (inF c); return $ Recv (fr @"Name" d) (fr @"Name" e) (outF g)
-  condBimapM p f j o@(Scop a b)   | p o = do c <- f (to @"Name" a);                        d <- j (inF b); return $ Scop (fr @"Name" c) (outF d)
-  condBimapM p f j o@(Comm a b)   | p o = do                               c <- j (inF a); d <- j (inF b); return $ Comm (outF c) (outF d)
-  condBimapM p f j o@(Serv a  )   | p o = do                                               b <- j (inF a); return $ Serv $ outF b
+  condBimapM p f j o@(Value a)    | p o = do b <- f (to @"Value" a);                                        return $ Value (fr @"Value" b)
+  condBimapM p f j o@(Send a b c) | p o = do d <- f (to @"Name"  a); e <- f (to @"Name" b); g <- j (inF c); return $ Send  (fr @"Name"  d) (fr @"Name" e) (outF g)
+  condBimapM p f j o@(Recv a b c) | p o = do d <- f (to @"Name"  a); e <- f (to @"Name" b); g <- j (inF c); return $ Recv  (fr @"Name"  d) (fr @"Name" e) (outF g)
+  condBimapM p f j o@(Scop a b)   | p o = do c <- f (to @"Name"  a);                        d <- j (inF b); return $ Scop  (fr @"Name"  c) (outF d)
+  condBimapM p f j o@(Comm a b)   | p o = do                                c <- j (inF a); d <- j (inF b); return $ Comm  (outF c) (outF d)
+  condBimapM p f j o@(Serv a  )   | p o = do                                                b <- j (inF a); return $ Serv $ outF b
   condBimapM p f j o = return $ cast o
    where
     cast :: ExprBF a b -> ExprBF c d
